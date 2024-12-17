8eb2c13cf654a3e7eb143148b220526beeb66b56
James Barrow <427722+jamiebarrow@users.noreply.github.com>
2024-12-17 10:58:03 +0100

    feat: execute file / line

--- init.lua
+++ init.lua
@@ -1,1 +1,6 @@
 print("jamiebarrow")
+
+-- Allow executing of whole file or selected line in a saved lua file.
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
