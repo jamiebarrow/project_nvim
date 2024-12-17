27a934adc67401c6af18ba87ac1f21f58311de1f
James Barrow <427722+jamiebarrow@users.noreply.github.com>
2024-12-17 16:26:23 +0100

    terminal

--- init.lua
+++ init.lua
@@ -50,3 +50,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Customize vim terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('jamiebarrow-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end
+})
+
+vim.keymap.set("n", "<leader>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+end)
